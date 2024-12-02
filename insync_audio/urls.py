6b51883dcff21784e91ee4c18b9b65ef9bf8c1e5
Sol Rayet <ge9i3fg8@students.codeinstitute.net>
2024-12-02 12:31:24 +0000

    Configure URLs

--- insync_audio/urls.py
+++ insync_audio/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('guest_mixes.urls'), name='home'),
+    path('', include('django.contrib.auth.urls')),
 ]
 
